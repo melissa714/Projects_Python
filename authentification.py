7d12eacd749c67111611761b25eb9362d952a5e5
melissa <kanangemelissanguessan@gmail.com>
2023-01-23 08:36:02 -1200

    authentification

---
+++ authentification.py
@@ -0,0 +1,19 @@
+
+import getpass
+
+authen={"melissa":"parfaite","gueu":"78geueP","parfaite":"parfaite","justin":"justin145","kanan":"parfait"}
+print(type(authen))
+
+user=str(input("Entrez votre nom d'utilisateur svp:"))
+
+pwd = getpass.getpass("entrez le mot de passe pour l'utilisateur %s : " % user)
+print(user, pwd)
+
+
+if user in authen and authen[user] == pwd:
+    print("super vous ètes connecté(e)  avec succes")
+else:
+    print("désolé les identifiant ne correspondent pas")
+
+
+    
